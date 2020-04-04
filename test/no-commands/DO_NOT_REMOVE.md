58ed72da099f35cfafa7dc6d8425eb3f080eee3d
Tyler Dodge <tylerdodge@Tylers-MacBook-Pro-2.local>
2020-04-04 11:29:25 -0700

    Add unit tests for entry points

---
+++ test/no-commands/DO_NOT_REMOVE.md
@@ -0,0 +1,1 @@
+This folder is here for the no-commands test
--- test/org-runbook-test.el
+++ test/org-runbook-test.el
@@ -5,4 +5,54 @@
   "Sanity check to make sure expected symbols are exported."
   (should (fboundp 'org-runbook-execute)))
 
+(ert-deftest org-runbook-execute-no-commands ()
+  "org-runbook-execute should throw an error when no commands are available"
+  (with-temp-buffer
+    (setq-local org-runbook-modes-directory "no-commands")
+    (setq-local org-runbook-project-directory "no-commands")
+    (should-error (org-runbook-execute))))
+
+(ert-deftest org-runbook-execute-one-command ()
+  "org-runbook-execute should execute the command referenced in the correspoding org file."
+  (with-temp-buffer
+    (setq-local org-runbook-modes-directory "one-command")
+    (setq-local org-runbook-project-directory "one-command")
+    (setq-local
+     completing-read-function
+     (lambda (prompt collection &optional predicate require-match initial-input hist def inherit-input-method)
+       (-> collection ht-keys first)))
+    (should (org-runbook-execute))))
+
+(ert-deftest org-runbook-view-one-command ()
+  "org-runbook-execute should execute the command referenced in the correspoding org file."
+  (with-temp-buffer
+    (setq-local org-runbook-modes-directory "one-command")
+    (setq-local org-runbook-project-directory "one-command")
+    (setq-local
+     completing-read-function
+     (lambda (prompt collection &optional predicate require-match initial-input hist def inherit-input-method)
+       (-> collection ht-keys first)))
+    (org-runbook-view)
+    (should (eq (get-buffer org-runbook-view-mode-buffer) (current-buffer)))
+    (goto-char (point-min))
+    (should (re-search-forward "echo test" nil t))
+    (org-runbook-view--open-at-point)
+    (should (s-contains-p "fundamental-mode.org" (buffer-file-name)))
+    (goto-char (point-min))
+    (should (re-search-forward "echo test" nil t))))
+
+(ert-deftest org-runbook-goto-one-command ()
+  "org-runbook-execute should execute the command referenced in the correspoding org file."
+  (with-temp-buffer
+    (setq-local org-runbook-modes-directory "one-command")
+    (setq-local org-runbook-project-directory "one-command")
+    (setq-local
+     completing-read-function
+     (lambda (prompt collection &optional predicate require-match initial-input hist def inherit-input-method)
+       (-> collection ht-keys first)))
+    (org-runbook-goto)
+    (should (s-contains-p "fundamental-mode.org" (buffer-file-name)))
+    (goto-char (point-min))
+    (should (re-search-forward "echo test" nil t))))
+
 (provide 'org-runbook-test)
