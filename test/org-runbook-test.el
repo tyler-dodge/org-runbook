9706ec47469593b48e54681c402759615a0076b2
Tyler Dodge <tylerdodge@Tylers-MacBook-Pro-2.local>
2020-04-04 11:35:47 -0700

    Fix test for travis

--- test/org-runbook-test.el
+++ test/org-runbook-test.el
@@ -5,18 +5,25 @@
   "Sanity check to make sure expected symbols are exported."
   (should (fboundp 'org-runbook-execute)))
 
+(defun relative-to-test-directory (file)
+  (->
+   (or (-some--> (and (f-exists-p "test") "test")
+         (f-join it file))
+       file)
+   expand-file-name))
+
 (ert-deftest org-runbook-execute-no-commands ()
   "org-runbook-execute should throw an error when no commands are available"
   (with-temp-buffer
-    (setq-local org-runbook-modes-directory "no-commands")
-    (setq-local org-runbook-project-directory "no-commands")
+    (setq-local org-runbook-modes-directory (relative-to-test-directory "no-commands"))
+    (setq-local org-runbook-project-directory (relative-to-test-directory "no-commands"))
     (should-error (org-runbook-execute))))
 
 (ert-deftest org-runbook-execute-one-command ()
   "org-runbook-execute should execute the command referenced in the correspoding org file."
   (with-temp-buffer
-    (setq-local org-runbook-modes-directory "one-command")
-    (setq-local org-runbook-project-directory "one-command")
+    (setq-local org-runbook-modes-directory (relative-to-test-directory "one-command"))
+    (setq-local org-runbook-project-directory (relative-to-test-directory "one-command"))
     (setq-local
      completing-read-function
      (lambda (prompt collection &optional predicate require-match initial-input hist def inherit-input-method)
@@ -26,8 +33,8 @@
 (ert-deftest org-runbook-view-one-command ()
   "org-runbook-execute should execute the command referenced in the correspoding org file."
   (with-temp-buffer
-    (setq-local org-runbook-modes-directory "one-command")
-    (setq-local org-runbook-project-directory "one-command")
+    (setq-local org-runbook-modes-directory (relative-to-test-directory "one-command"))
+    (setq-local org-runbook-project-directory (relative-to-test-directory "one-command"))
     (setq-local
      completing-read-function
      (lambda (prompt collection &optional predicate require-match initial-input hist def inherit-input-method)
@@ -44,8 +51,8 @@
 (ert-deftest org-runbook-goto-one-command ()
   "org-runbook-execute should execute the command referenced in the correspoding org file."
   (with-temp-buffer
-    (setq-local org-runbook-modes-directory "one-command")
-    (setq-local org-runbook-project-directory "one-command")
+    (setq-local org-runbook-modes-directory (relative-to-test-directory "one-command"))
+    (setq-local org-runbook-project-directory (relative-to-test-directory "one-command"))
     (setq-local
      completing-read-function
      (lambda (prompt collection &optional predicate require-match initial-input hist def inherit-input-method)
