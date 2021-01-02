5383b42e8f0d64a7111b18773bcde1b71428a9db
Tyler Dodge <tylerdodge@Tylers-MacBook-Pro-2.local>
2021-01-02 00:07:24 -0800

    Undo cache attempt

--- setup-travis.sh
+++ setup-travis.sh
@@ -3,11 +3,7 @@ set -o errexit
 set -o pipefail
 set -o nounset
 EMACS_MAJOR_VERSION="$(echo $EMACS_VERSION | cut -d '.' -f 1)"
-if  [ -d "$HOME/emacs" ]; then
-    emacs --version
-    echo emacs already installed.
-    exit
-fi
+
 if [ $EMACS_MAJOR_VERSION = 27 -o $EMACS_MAJOR_VERSION = 26 ]; then
     make -f emacs-travis.mk install_gnutls
 fi
