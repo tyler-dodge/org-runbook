b1c24a0458f74c88d7c1c7335578956d31f9fe76
Tyler Dodge <tylerdodge@Tylers-MacBook-Pro-2.local>
2021-01-01 19:11:09 -0800

    Add Cask version for emacs 26

--- setup-cask.sh
+++ setup-cask.sh
@@ -6,6 +6,8 @@ set -o nounset
 EMACS_MAJOR_VERSION="$(echo "$EMACS_VERSION" | cut -f 1 -d '.')"
 if [ "$EMACS_MAJOR_VERSION" = "27" ];then
     cp Cask-27 Cask
-else
+elif [ "$EMACS_MAJOR_VERSION" = "26" ];then
+    cp Cask-26 Cask
+else 
     cp Cask-default Cask
 fi
