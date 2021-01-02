823f9ddc6fa96402c146a231129abf7970e0a151
Tyler Dodge <tylerdodge@Tylers-MacBook-Pro-2.local>
2021-01-02 07:40:49 -0800

    Include makefile directly for modification

--- setup-travis.sh
+++ setup-travis.sh
@@ -5,7 +5,6 @@ set -o nounset
 EMACS_MAJOR_VERSION="$(echo $EMACS_VERSION | cut -d '.' -f 1)"
 
 if [ $EMACS_MAJOR_VERSION = 27 -o $EMACS_MAJOR_VERSION = 26 ]; then
-    make -f emacs-travis.mk install_gnutls
 fi
 make -f emacs-travis.mk install_emacs
 make -f emacs-travis.mk install_cask
